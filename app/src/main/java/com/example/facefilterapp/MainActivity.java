0a9c9c53565b519d900fcc96919286ab8469de41
PRABHU-OFFICIAL <lituplayer@gmail.com>
2022-09-27 23:28:54 +0530

    completed

---
+++ app/src/main/java/com/example/facefilterapp/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.facefilterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Build;
+import android.os.Bundle;
+import com.google.ar.core.AugmentedFace;
+import com.google.ar.core.Frame;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.Renderable;
+import com.google.ar.sceneform.rendering.Texture;
+import com.google.ar.sceneform.ux.AugmentedFaceNode;
+import java.util.Collection;
+
+public class MainActivity extends AppCompatActivity {
+
+	private ModelRenderable modelRenderable;
+	private Texture texture;
+	private boolean isAdded = false;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		CustomArFragment customArFragment = (CustomArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+			ModelRenderable
+					.builder()
+					.setSource(this , R.raw.fox_face)
+					.build()
+					.thenAccept(renderable -> {
+						modelRenderable = renderable;
+						modelRenderable.setShadowCaster(false);
+						modelRenderable.setShadowReceiver(false);
+					});
+			Texture.builder()
+					.setSource(this , R.drawable.fox_face_mesh_texture)
+					.build()
+					.thenAccept(texture -> this.texture = texture);
+		}
+
+		assert false;
+		customArFragment.getArSceneView().setCameraStreamRenderPriority(Renderable.RENDER_PRIORITY_FIRST);
+		customArFragment.getArSceneView().getScene().addOnUpdateListener(FrameTime -> {
+			if (modelRenderable == null || texture == null){
+				return;
+			}
+			Frame frame = customArFragment.getArSceneView().getArFrame();
+			Collection<AugmentedFace> augmentedFaces = frame.getUpdatedTrackables(AugmentedFace.class);
+			for (AugmentedFace augmentedFace : augmentedFaces){
+				if (isAdded){
+					return;
+				}
+				AugmentedFaceNode augmentedFaceNode = new AugmentedFaceNode(augmentedFace);
+				augmentedFaceNode.setParent(customArFragment.getArSceneView().getScene());
+				augmentedFaceNode.setFaceMeshTexture(texture);
+				isAdded = true;
+			}
+		});
+	}
+}
